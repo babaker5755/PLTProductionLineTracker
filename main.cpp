194a0fcf1421669d5f242eac0ee4d5aa1b7ab061
Brandon <bbrandonbaker.b@gmail.com>
2019-05-16 22:09:17 -0400

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+int main() {
+
+    std::cout << "Welcome to PLT" << std::endl;
+    std::cout << std::endl;
+    std::cout << "1. Add Employee Account" << std::endl;
+    std::cout << "2. Add Music Player" << std::endl;
+    std::cout << "3. Add Movie Player" << std::endl;
+    std::cout << "4. Display Production Statistics" << std::endl;
+    std::cout << "5. Exit" << std::endl;
+
+    return 0;
+}
